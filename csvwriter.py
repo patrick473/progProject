dfad896b00c45d707912a46df13e157732f3bdfb
Pim <pim_rheiter@live.nl>
2016-11-08 11:02:03 +0100

    test

--- csvwriter.py
+++ csvwriter.py
@@ -19,4 +19,6 @@ def fietsToevoegen(invoer):
         for row in rows:
               writer.writerow((row[0], row[1], row[2], row[3], row[4], row[5],
                                row[6], row[7]))
+
+# TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
               
