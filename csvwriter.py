6d5c6039e7fa1c455ccdaf11db5924c1507d4b7f
patrick kottman <patkottman@gmail.com>
2016-11-09 13:50:44 +0100

    Add files via upload
    csvwriter gefixt

--- csvwriter.py
+++ csvwriter.py
@@ -84,17 +84,3 @@ def checkLogin(fietsNummer, password):
         else:
             return False
 
-def checkLogin(fietsNummer, password):
-    try:
-        rows = []
-        with open('fietsen.csv', 'r', newline = '') as myCSVFile:
-            reader = csv.reader(myCSVFile, delimiter = ';')
-            for row in reader:
-                rows.append(row)
-    except FileNotFoundError:
-        return False
-    for row in rows:
-        if row[6] == password and row[7] == fietsNummer:
-            return True
-        else:
-            return False
